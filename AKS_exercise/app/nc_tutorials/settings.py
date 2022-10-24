22b750f0c20fa0c537bba1c7c2f0df297c74d9a0
Fidencio Guerra <107739967+Fidencio-Codes@users.noreply.github.com>
2022-10-23 23:11:35 -0700

    Update settings.py

--- AKS_exercise/app/nc_tutorials/settings.py
+++ AKS_exercise/app/nc_tutorials/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l9suwi+ox!o8&tmo)3_uqn9syi$r01a=#s0zvpjot-_u1ovi##'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
